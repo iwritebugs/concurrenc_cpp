9cdba4ffdce8ae8a5f0ed8c0fc8932398bde5508
Viktor Holmelin <viktor.holmelin@crunchfish.com>
2020-06-23 06:46:08 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/concurrenc_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/concurrenc_cpp.dir/main.cpp.o"
+  "concurrenc_cpp"
+  "concurrenc_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/concurrenc_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
